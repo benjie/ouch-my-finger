3d9015c136f9a85ce87110f14ef96a8cce18119d
Benjie Gillam <benjie@jemjie.com>
2023-12-15 09:51:29 +0000

    Copy setup from Litewarp on Discord

--- graphile.config.mjs
+++ graphile.config.mjs
@@ -1,44 +1,90 @@
 // @ts-check
 import { makePgService } from "@dataplan/pg/adaptors/pg";
-import AmberPreset from "postgraphile/presets/amber";
+import { PostGraphileAmberPreset } from "postgraphile/presets/amber";
 import { makeV4Preset } from "postgraphile/presets/v4";
 import { PostGraphileConnectionFilterPreset } from "postgraphile-plugin-connection-filter";
 import { PgAggregatesPreset } from "@graphile/pg-aggregates";
 import { PgManyToManyPreset } from "@graphile-contrib/pg-many-to-many";
-// import { PgSimplifyInflectionPreset } from "@graphile/simplify-inflection";
+import { PgSimplifyInflectionPreset } from "@graphile/simplify-inflection";
+import { PostGraphileRelayPreset } from "postgraphile/presets/relay";
+import { custom as makePgOmitArchivedPlugin } from "@graphile-contrib/pg-omit-archived";
+import PersistedPlugin from "@grafserv/persisted";
 
-// For configuration file details, see: https://postgraphile.org/postgraphile/next/config
+const isProduction = false;
 
-/** @satisfies {GraphileConfig.Preset} */
+/** @type {GraphileConfig.Preset} */
 const preset = {
   extends: [
-    AmberPreset.default ?? AmberPreset,
-    makeV4Preset({
-      /* Enter your V4 options here */
-      graphiql: true,
-      graphiqlRoute: "/",
-    }),
+    PostGraphileAmberPreset,
+    PostGraphileRelayPreset,
+    PgSimplifyInflectionPreset,
+    PgAggregatesPreset,
     PostGraphileConnectionFilterPreset,
     PgManyToManyPreset,
-    PgAggregatesPreset,
-    // PgSimplifyInflectionPreset
-  ],
-  pgServices: [
-    makePgService({
-      // Database connection string:
-      connectionString: process.env.DATABASE_URL,
-      // List of schemas to expose:
-      schemas: process.env.DATABASE_SCHEMAS?.split(",") ?? ["public"],
-      // Enable LISTEN/NOTIFY:
-      pubsub: true,
+    makeV4Preset({
+      // dynamicJson: true,
+      // ignoreRBAC: false,
+      // setofFunctionsContainNulls: false,
+      // subscriptions: true,
+      //   graphileBuildOptions: {
+      // nestedMutationsSimpleFieldNames: true,
+      // nestedMutationsDeleteOthers: true,
+      //   },
     }),
   ],
-  grafserv: {
-    port: 5678,
-    websockets: true,
+
+  gather: {
+    // pgStrictFunctions: true,
+    installWatchFixtures: false, // need to install manually
   },
+
   grafast: {
-    explain: true,
+    // context: async (ctx, args) => {
+    //   return {};
+    // },
+  },
+
+  grafserv: {
+    allowUnpersistedOperation: (event) => {
+      return !isProduction;
+      //&& !!event.request?.getHeader("referer")?.endsWith("/graphiql")
+    },
+    // persistedOperations: { ...persistedOperations },
+    // port: process.env["PORT"] ? Number(process.env["PORT"]) : 4001,
+    ...(isProduction
+      ? {}
+      : {
+          graphiql: true,
+          graphiqlPath: "/graphiql",
+          watch: true,
+        }),
+  },
+
+  inflection: {},
+
+  plugins: [makePgOmitArchivedPlugin("archived"), PersistedPlugin.default],
+
+  pgServices: [
+    makePgService({
+      connectionString: process.env["DATABASE_URL"] ?? "postgres:///litewarp",
+      schemas: ["app_public", "app_private", "public", "case_mgmt"],
+    }),
+  ],
+  schema: {
+    ...(isProduction
+      ? { retryOnInitFail: true }
+      : {
+          exportSchemaSDLPath: "schema.graphql",
+          sortExport: true,
+          // dontSwallowErrors: true,
+        }),
+    connectionFilterRelations: true,
+    connectionFilterAllowNullInput: true,
+    connectionFilterAllowEmptyObjectInput: true,
+    pgArchivedColumnName: "archived_at",
+    pgArchivedRelations: true,
+    pgArchivedDefault: "NO",
+    pgJwtSecret: process.env.JWT_SECRET ?? "",
   },
 };
 
