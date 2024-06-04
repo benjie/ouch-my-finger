146b619f0b5e91ff44f7a0a40666259c59772ed9
Benjie Gillam <benjie@jemjie.com>
2024-06-04 14:12:43 +0100

    Fix EOL

--- FooSubscriptionPlugin.ts
+++ FooSubscriptionPlugin.ts
@@ -7,14 +7,14 @@ export const FooSubscriptionPlugin = makeExtendSchemaPlugin((build) => {
   const { foo, auth_can_access } = build.input.pgRegistry.pgResources;
   return {
     typeDefs: gql`
-        extend type Subscription {
-            foo(fooId: Int!): FooSubscriptionPayload
-        }
+      extend type Subscription {
+        foo(fooId: Int!): FooSubscriptionPayload
+      }
 
-        type FooSubscriptionPayload {
-            event: String
-            foo: Foo
-        }
+      type FooSubscriptionPayload {
+        event: String
+        foo: Foo
+      }
     `,
     plans: {
       Subscription: {
@@ -22,25 +22,28 @@ export const FooSubscriptionPlugin = makeExtendSchemaPlugin((build) => {
           subscribePlan(_$root, args) {
             const $pgSubscriber = context().get("pgSubscriber");
             const $fooId = args.get("fooId");
-            const $auth = (auth_can_access.execute() as PgSelectSingleStep).record();
+            const $auth = (
+              auth_can_access.execute() as PgSelectSingleStep
+            ).record();
             const $topic = lambda(
               list([$fooId, $auth]),
               (records: [number, { can_access: boolean }]) => {
                 const [foo_id, has_access, foo] = records;
-                console.log('LAMBDA RESOLVE', records);
+                console.log("LAMBDA RESOLVE", records);
                 if (!has_access.can_access) {
                   throw new Error("Unauthorized");
                 }
 
                 return `foo:${foo_id}`;
-              });
+              }
+            );
 
             return listen($pgSubscriber, $topic, jsonParse);
           },
           plan($event) {
             return $event;
-          }
-        }
+          },
+        },
       },
       FooSubscriptionPayload: {
         event($event) {
@@ -48,8 +51,8 @@ export const FooSubscriptionPlugin = makeExtendSchemaPlugin((build) => {
         },
         foo($event) {
           return foo.get({ foo_id: $event.get("subject") });
-        }
-      }
-    }
+        },
+      },
+    },
   };
 });
