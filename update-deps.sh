5f31e3ad51db84c657380ed0c16c6f7fce77ab95
Benjie Gillam <benjie@jemjie.com>
2025-06-07 10:47:58 +0100

    Add update-deps script

---
+++ update-deps.sh
@@ -0,0 +1,2 @@
+yarn add @graphile-contrib/pg-many-to-many@beta @graphile/pg-aggregates@beta @graphile/simplify-inflection@beta graphile@beta postgraphile@beta postgraphile-plugin-connection-filter@beta @grafserv/persisted@beta @graphile-contrib/pg-omit-archived@beta
+
