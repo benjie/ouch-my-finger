8c59f1e871b7e348ca006e19d8ee78283063c78f
Benjie Gillam <benjie@jemjie.com>
2025-07-16 09:55:33 +0100

    Add dedupe to update script

--- update-deps.sh
+++ update-deps.sh
@@ -1,2 +1,3 @@
 yarn add @graphile-contrib/pg-many-to-many@beta @graphile/pg-aggregates@beta @graphile/simplify-inflection@beta graphile@beta postgraphile@beta postgraphile-plugin-connection-filter@beta @grafserv/persisted@beta @graphile-contrib/pg-omit-archived@beta @benjie/tsconfig
+yarn dedupe
 
